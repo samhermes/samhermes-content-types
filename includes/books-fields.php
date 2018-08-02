a51b9aa05fd3d23ad2e78f4bbe24aea4b4c26140
Sam Hermes <sam.hermes@wustl.edu>
2018-08-02 18:10:58 -0500

    Switch field post type to books

--- includes/books-fields.php
+++ includes/books-fields.php
@@ -31,7 +31,7 @@ if ( function_exists( 'acf_add_local_field_group' ) ) {
                 array(
                     'param' => 'post_type',
                     'operator' => '==',
-                    'value' => 'post',
+                    'value' => 'books',
                 ),
             ),
         ),
