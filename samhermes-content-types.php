b25e21dd5ae9343908070c0ca8cc907f258e5fa3
Sam Hermes <sam.hermes@wustl.edu>
2018-08-09 12:13:56 -0500

    Remove GraphQL fields

--- samhermes-content-types.php
+++ samhermes-content-types.php
@@ -20,27 +20,3 @@ function samhermes_acf_init() {
 	acf_update_setting( 'google_api_key', 'AIzaSyCRwBjaYzaXEbPTJEl4MjVqtNHtHO9fkwc' );
 }
 add_action('acf/init', 'samhermes_acf_init');
-
-function samhermes_locations_graphql_fields( $fields ) {
-	$fields['coordinates'] = [
-        'type' => \WPGraphQL\Types::string(),
-        'description' => 'The map coordinates.',
-        'resolve' => function( \WP_Post $location ) {
-            $coordinates = get_post_meta( $location->ID, 'coordinates', true );
-            $coordinates = '[' . $coordinates['lat'] . ', ' . $coordinates['lng'] . ']';
-            return ! empty( $coordinates ) ? esc_html( $coordinates ) : null;
-        },
-    ];
-
-    $fields['visits'] = [
-        'type' => \WPGraphQL\Types::string(),
-        'description' => 'The number of visits.',
-        'resolve' => function( \WP_Post $location ) {
-            $visits = get_post_meta( $location->ID, 'number_of_visits', true );
-            return ! empty( $visits ) ? esc_html( $visits ) : null;
-        },
-    ];
-
-	return $fields;
-}
-add_filter( 'graphql_location_fields', 'samhermes_locations_graphql_fields', 99, 1 );
